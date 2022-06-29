409a7ca11f05612679d21779562a415f5e0facd1
ZaneleMoni <zanelemoni4@gmail.com>
2022-06-29 09:31:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Carousel
+ * Description:       my first PHP Tutorial
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ntombizanele Moni and Haniah Jarden
+*/
+?>
